7595bd39cb92ea6956eb2dadf434f1d0cc9a43ca
ahabanova <ahabanova95@gmail.com>
2023-05-18 14:05:41 +0200

    Spousta změn - oddělěné komponenty

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('funguju!');
+const mainElm = document.querySelector('main')
+
+fetch('https://nakupy.kodim.app/api/sampleweek/mon')
+  .then((response) => response.json())
+  .then((data) => {
+    mainElm.innerHTML += ShopList({
+      dayName: data.result.dayName,
+      items: data.result.items
+    })
+  });
+
+fetch('https://nakupy.kodim.app/api/sampleweek/tue')
+  .then((response) => response.json())
+  .then((data) => {
+    mainElm.innerHTML += ShopList({
+      dayName: data.result.dayName,
+      items: data.result.items
+    })
+  });
